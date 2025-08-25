417574e0498278c06dd4d4c243810105c2ffd49e
Natsuda Rongthong <664259006@webmail.npru.ac.th>
2025-08-25 17:14:42 +0700

    update

--- server/routers/restaurant.router.js
+++ server/routers/restaurant.router.js
@@ -1,22 +1,23 @@
 import express from "express";
 import restaurantControllers from "../controllers/restaurant.controllers.js";
+import authMiddleware from "../middleware/authjwt.js";
 
 const router = express.Router();
 
 // POST http://localhost:5000/api/v1/restaurants
-router.post("/", restaurantControllers.create);
+router.post("/",authMiddleware.verifyToken,authMiddleware.isModOrAdmin, restaurantControllers.create);
 
 // GET http://localhost:5000/api/v1/restaurants
-router.get("/", restaurantControllers.getAll);
+router.get("/",authMiddleware.verifyToken, restaurantControllers.getAll);
 
 // GET http://localhost:5000/api/v1/restaurants/:id
-router.get("/:id", restaurantControllers.getById);
+router.get("/:id", authMiddleware.verifyToken,restaurantControllers.getById);
 
 // PUT http://localhost:5000/api/v1/restaurants/:id
-router.put("/:id", restaurantControllers.update);
+router.put("/:id", authMiddleware.verifyToken,authMiddleware.isModOrAdmin,restaurantControllers.update);
 
 // DELETE http://localhost:5000/api/v1/restaurants/:id
-router.delete("/:id", restaurantControllers.deleteById);
+router.delete("/:id",authMiddleware.verifyToken,authMiddleware.isAdmin, restaurantControllers.deleteById);
 
 export default router;
 
