7d1e05e828dfaa8d0fee908f556b36d77389df69
Natsuda006 <156069062+Natsuda006@users.noreply.github.com>
2025-08-31 20:43:06 +0700

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,7 +5,6 @@ import RestaurantService from "../service/restautant.service";
 import Swal from "sweetalert2";
 
 const Home = () => {
- feature/authentication
   const [restaurants, setRestaurants] = useState([]);
   const [filterRestaurants, setFilterRestaurants] = useState([]);
 
@@ -16,17 +15,6 @@ const Home = () => {
     }
 
     const result = restaurants.filter((restaurant) =>
-
-const [restaurants, setRestaurants] = useState([]);
-const [filterRestaurants, setFilterRestaurants] = useState([]);
-const handleSearch = (keyword) =>{
-  if(keyword === ""){
-      setFilterRestaurants(restaurants)
-    return;
-  }
-  const result = restaurants.filter((restaurant) =>{
-    return (
- main
       restaurant.title.toLowerCase().includes(keyword.toLowerCase()) ||
       restaurant.type.toLowerCase().includes(keyword.toLowerCase())
     );
@@ -34,7 +22,6 @@ const handleSearch = (keyword) =>{
   };
 
   useEffect(() => {
- feature/authentication
     const getAllRestaurants = async () => {
       try {
         const data = await RestaurantService.getAllRestaurants();
@@ -54,21 +41,6 @@ const handleSearch = (keyword) =>{
     getAllRestaurants();
   }, []);
 
-
-    //call api: getAllRestaurants
-    fetch("http://localhost:5000/api/v1/restaurants/").then((res)=>{
-      //convert to json
-      return res.json()
-    }).then((Response)=>{
-      //save to state
-      setRestaurants(Response)
-      setFilterRestaurants(Response)
-    }).catch((err)=> {
-      //catch error
-      console.log(err.message);
-    }); 
-  },[])
- main
   return (
     <div className="container mx-auto">
       <Navbar />
@@ -91,16 +63,12 @@ const handleSearch = (keyword) =>{
               <path d="m21 21-4.3-4.3"></path>
             </g>
           </svg>
- feature/authentication
           <input
             type="search"
             name="keyword"
             onChange={(e) => handleSearch(e.target.value)}
             placeholder="Search"
           />
-
-          <input type="search" name = "keyword" onChange={(e) => handleSearch(e.target.value)} required placeholder="Search"/>
- main
         </label>
       </div>
       <Restaurant restaurants={filterRestaurants} />
