fad0d3a81ed24c94811dd6a19847fa1b82e4527f
Natsuda Rongthong <664259006@webmail.npru.ac.th>
2025-09-01 14:18:57 +0700

    up indexx

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ import authRouter from "./routers/auth.routers.js";
 import cors from "cors";
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  origin: ["http://localhost:5173", "http://127.0.0.1:5173",FRONTEND_URL],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "x-access-token"],
 }));
