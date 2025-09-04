82229ef85a84ca8ced937c58405f6ce2fc3c58b0
Natsuda Rongthong <664259006@webmail.npru.ac.th>
2025-09-04 09:07:55 +0700

    up

--- server/routers/auth.routers.js
+++ server/routers/auth.routers.js
@@ -2,10 +2,10 @@ import express from "express";
 const router = express.Router();
 import authControllers from "../controllers/auth.controllers.js";
 
-// POST http://localhost:5001/api/v1/auth/signup
+// POST http://localhost:5000/api/v1/auth/signup
 router.post("/signup", authControllers.signUp);
 
-// POST http://localhost:5001/api/v1/auth/signin
+// POST http://localhost:5000/api/v1/auth/signin
 router.post("/signin", authControllers.signIn);
 
 export default router;
