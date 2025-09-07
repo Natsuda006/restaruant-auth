bed17306849da6c9475a7767c36c6f0af8258bd7
Natsuda Rongthong <664259006@webmail.npru.ac.th>
2025-09-07 20:32:32 +0700

    update index

--- server/index.js
+++ server/index.js
@@ -3,14 +3,13 @@ import dotenv from "dotenv";
 import cors from "cors";
 
 import restaurantRouter from "./routers/restaurant.router.js";
+import authRouter from "./routers/auth.routes.js";  // ✅ import
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
 const FRONTEND_URL = process.env.FRONTEND_URL || "https://restaruant-auth3.vercel.app";
-
 
 app.use(cors({
   origin: ["http://localhost:5173", "http://127.0.0.1:5173", FRONTEND_URL],
@@ -21,15 +20,13 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.get("/", (req, res) => {
   res.send("Restaurant Restful API");
 });
 
-
 app.use("/api/v1/restaurants", restaurantRouter);
-
+app.use("/api/v1/auth", authRouter);   // ✅ เพิ่มบรรทัดนี้
 
 app.listen(PORT, () => {
-    console.log(`Listening on http://localhost:${PORT}`);
+  console.log(`Listening on http://localhost:${PORT}`);
 });
