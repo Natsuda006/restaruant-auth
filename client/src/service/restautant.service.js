017cc72a7e6376e254c1f91e06eb1fa183456f06
Natsuda Rongthong <664259006@webmail.npru.ac.th>
2025-09-07 23:23:11 +0700

    upFrontend

--- client/src/service/restautant.service.js
+++ client/src/service/restautant.service.js
@@ -1,28 +1,32 @@
 import api from "./api";
-const RESTO_API = import.meta.env.VITE_RESTO_API;
+const RESTO_API = import.meta.env.VITE_RESTO_API; 
+
+const getAuthHeader = () => ({
+  headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }
+});
 
 const getAllRestaurants = async () => {
-  const response = await api.get(RESTO_API);
+  const response = await api.get(RESTO_API, getAuthHeader());
   return response.data;
 };
 
 const getRestaurantById = async (id) => {
-  const response = await api.get(`${RESTO_API}/${id}`);
+  const response = await api.get(`${RESTO_API}/${id}`, getAuthHeader());
   return response.data;
 };
 
 const editRestaurant = async (id, restaurant) => {
-  const response = await api.put(`${RESTO_API}/${id}`, restaurant);
+  const response = await api.put(`${RESTO_API}/${id}`, restaurant, getAuthHeader());
   return response.data;
 };
 
 const insertRestaurant = async (restaurant) => {
-  const response = await api.post(RESTO_API, restaurant);
+  const response = await api.post(RESTO_API, restaurant, getAuthHeader());
   return response.data;
 };
 
 const deleteRestaurant = async (id) => {
-  const response = await api.delete(`${RESTO_API}/${id}`);
+  const response = await api.delete(`${RESTO_API}/${id}`, getAuthHeader());
   return response.data;
 };
 
